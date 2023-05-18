da7e9627dc46b84fb94c4759b0c90f25c9cf9d88
git-savio1.domi <techsavvydominic3@gmail.com>
2023-05-18 09:22:09 +0300

    Completed assignments

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+    def initialize word
+      @word = word
+    end
+    def match item
+        item.select do |new_word|
+            new_word.split('').sort == @word.split('').sort
+        end
+    end
+end
